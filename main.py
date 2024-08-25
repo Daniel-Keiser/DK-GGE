cf4cd8978501e87cb46cc5eddb8646e82d8f1206
Daniel Strey <daniel.ks100@gmail.com>
2024-08-25 19:05:15 -0300

    stile

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ app = FastAPI()
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
 csv_file_path = 'static/output.csv'
+temp_csv_file_path = 'static/output_temp.csv'  # Caminho do arquivo temporário
 
 def fetch_data(url):
     try:
@@ -64,7 +65,7 @@ def fetch_data_for_looting(base_url, loot_limit, collected_data):
             else:
                 print("No relevant data found in this batch, continuing...")
             current_index += 1
-            time.sleep(1)
+            #time.sleep(1)
         else:
             print("No more data to fetch from the current index.")
             break
@@ -73,17 +74,21 @@ def generate_csv(loot_limit):
     base_url = 'https://empire-api.fly.dev/EmpireEx_21/hgh/'
     all_data = []
 
-    fetch_data_for_looting(base_url, loot_limit, all_data)
-    df = pd.DataFrame(all_data, columns=['Loot ID', 'Name'])
-    df.to_csv(csv_file_path, index=False, sep=',', encoding='utf-8')
+    try:
+        fetch_data_for_looting(base_url, loot_limit, all_data)
+        df = pd.DataFrame(all_data, columns=['Loot ID', 'Name'])
+        df.to_csv(temp_csv_file_path, index=False, sep=',', encoding='utf-8')
+        
+        # Substituir o CSV antigo pelo novo
+        if os.path.exists(temp_csv_file_path):
+            os.replace(temp_csv_file_path, csv_file_path)  # Renomeia o arquivo temporário para o nome final
+    except Exception as e:
+        print(f"Failed to generate CSV: {e}")
+        # Mantém o arquivo CSV antigo intacto
 
 @app.post("/run-task/")
 async def run_task(background_tasks: BackgroundTasks, loot_limit: int = Query(5000000, description="The loot limit value")):
-    # Remove old CSV if it exists
-    if os.path.exists(csv_file_path):
-        os.remove(csv_file_path)
-    
-    # Run the task in the background
+    # Executar a tarefa em segundo plano
     background_tasks.add_task(generate_csv, loot_limit)
     return JSONResponse({"message": f"Task started with loot limit {loot_limit}. CSV is being generated."})
 
